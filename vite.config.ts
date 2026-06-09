700ead377ea1408d667fca9047ac7da14f62ee60
sawantakshata27 <sawantakshata27@gmail.com>
2026-06-09 16:56:24 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
@@ -15,6 +16,7 @@ export default defineConfig({
     tsConfigPaths(),
     tailwindcss(),
     tanstackStart(),
+    netlify(),
     viteReact(),
   ],
 });

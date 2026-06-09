d35fcca19f38cab38f3078dbf5ea65b4cd12c837
sawantakshata27 <sawantakshata27@gmail.com>
2026-06-09 17:00:04 +0530

    deply

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
-    allowedHosts: true,
-  },
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
     tanstackStart(),
-    netlify(),
     viteReact(),
+    netlify(), // keep this last
   ],
 });
